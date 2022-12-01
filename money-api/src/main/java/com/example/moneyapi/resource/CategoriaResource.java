b8413b8ee753e521a920e70ca8c437032f3d34c1
Jose Guilherme Benites Machado <jgbm81@hotmail.com>
2022-12-01 14:43:55 -0400

    post criar categoria e get recuperar por codigo

--- money-api/src/main/java/com/example/moneyapi/resource/CategoriaResource.java
+++ money-api/src/main/java/com/example/moneyapi/resource/CategoriaResource.java
@@ -1,11 +1,23 @@
 package com.example.moneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.Entity;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.moneyapi.model.Categoria;
 import com.example.moneyapi.repository.CategoriaRepository;
@@ -22,6 +34,21 @@ public class CategoriaResource {
 		return categoriaRepository.findAll();
 	}
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		 Optional<Categoria> categoria = categoriaRepository.findById(codigo);
+		 return categoria.isPresent() ? ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
+	}
 	
 	
 }
