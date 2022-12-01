5795bc94b711d7e88ad02ede5a2124d03b37312e
Jose Guilherme Benites Machado <jgbm81@hotmail.com>
2022-12-01 14:13:55 -0400

    first commit

---
+++ money-api/src/main/java/com/example/moneyapi/repository/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.example.moneyapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.moneyapi.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+	
+	
+}
