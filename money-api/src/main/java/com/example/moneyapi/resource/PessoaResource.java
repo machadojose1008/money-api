6e640d268941c8cee110fd2da89d6100bd2df658
Jose Machado <jgbm81@hotmail.com>
2022-12-08 13:43:59 -0400

    Criação Delete de Pessoas

--- money-api/src/main/java/com/example/moneyapi/resource/PessoaResource.java
+++ money-api/src/main/java/com/example/moneyapi/resource/PessoaResource.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,7 +54,13 @@ public class PessoaResource {
 	@GetMapping("/{id}")
 	public ResponseEntity<Pessoa> buscarPeloid(@PathVariable Long id){
 		Optional<Pessoa> pessoa = pessoaRepository.findById(id);
-		return pessoa != null ? ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build();
+		return pessoa.isPresent() ? ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build();
 	}
 	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long id) {
+		pessoaRepository.deleteById(id);
+		
+	}
 }
