7e87eb0e369429d53cee138e687615d3069e8530
Jose Machado <jgbm81@hotmail.com>
2022-12-08 15:24:23 -0400

    Criação do PUT para atualizar Pessoas

--- money-api/src/main/java/com/example/moneyapi/resource/PessoaResource.java
+++ money-api/src/main/java/com/example/moneyapi/resource/PessoaResource.java
@@ -7,14 +7,17 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -24,6 +27,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.example.moneyapi.event.RecursoCriadoEvent;
 import com.example.moneyapi.model.Pessoa;
 import com.example.moneyapi.repository.PessoaRepository;
+import com.example.moneyapi.service.PessoaService;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -36,6 +40,9 @@ public class PessoaResource {
 	public List<Pessoa> listar() {
 		return pessoaRepository.findAll();
 	}
+	
+	@Autowired
+	private PessoaService pessoaService;
 	
 	@Autowired
 	private ApplicationEventPublisher publisher;
@@ -61,6 +68,14 @@ public class PessoaResource {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long id) {
 		pessoaRepository.deleteById(id);
+		
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long id, @Valid @RequestBody Pessoa pessoa){
+		
+		Pessoa pessoaSalva = pessoaService.atualizar(id, pessoa);
+		return ResponseEntity.ok(pessoaSalva);
 		
 	}
 }
---
+++ money-api/src/main/java/com/example/moneyapi/service/PessoaService.java
@@ -0,0 +1,30 @@
+package com.example.moneyapi.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.example.moneyapi.model.Pessoa;
+import com.example.moneyapi.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Pessoa atualizar(Long id,Pessoa pessoa) {
+		Pessoa pessoaSalva = this.pessoaRepository.findById(id)
+				.orElseThrow(() -> new EmptyResultDataAccessException(1));
+			
+			if(pessoaSalva == null) {
+				throw new EmptyResultDataAccessException(1);
+			}
+			
+			BeanUtils.copyProperties(pessoa, pessoaSalva,"id");
+			
+			return pessoaRepository.save(pessoaSalva);
+	}
+	
+}
